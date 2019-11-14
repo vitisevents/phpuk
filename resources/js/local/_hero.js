4f4065d50f865d39ef01257c42c07c9d3a1acbe3
Nico van Zyl <nico@wixelhq.com>
2019-11-14 18:03:29 +0200

    Add video tabbed nav

--- resources/js/local/_hero.js
+++ resources/js/local/_hero.js
@@ -1,10 +1,14 @@
-var slider = tns({
-  container: ".hero-slider",
-  mode: "gallery",
-  items: 1,
-  slideBy: "page",
-  autoplay: true,
-  controls: false,
-  speed: 5000,
-  autoplayButtonOutput: false
-});
+var hero = document.querySelector(".hero-slider");
+
+if (hero) {
+  var slider = tns({
+    container: ".hero-slider",
+    mode: "gallery",
+    items: 1,
+    slideBy: "page",
+    autoplay: true,
+    controls: false,
+    speed: 5000,
+    autoplayButtonOutput: false
+  });
+}
