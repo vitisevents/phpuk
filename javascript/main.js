2ce9eb177892ffd07eab2feb6577e91f09144fcf
Nico van Zyl <nicovz92@gmail.com>
2019-11-17 14:53:01 +0200

    Improve hero slider

--- javascript/main.js
+++ javascript/main.js
@@ -112,12 +112,12 @@ var hero = document.querySelector(".hero-slider");
 if (hero) {
   var slider = tns({
     container: ".hero-slider",
-    mode: "gallery",
+    // mode: "gallery",
     items: 1,
     slideBy: "page",
     autoplay: true,
     controls: false,
-    speed: 5000,
+    speed: 350,
     autoplayButtonOutput: false
   });
 }
