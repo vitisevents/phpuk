76432c4b68c48a173b00e127b0deb92bbedd1d03
Nico van Zyl <nicovz92@gmail.com>
2019-11-20 15:33:03 +0200

    Responsive header

--- javascript/main.js
+++ javascript/main.js
@@ -73,40 +73,40 @@ if (clock) {
 var lastKnownScrollPosition = 0;
 var ticking = false;
 var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-var header = document.querySelector(".header");
+var header = document.querySelector('.header');
 
 function toggleHeaderClasses(type) {
-  if (type == "add") {
-    header.classList.remove("absolute", "text-white");
-    header.classList.add("fixed", "bg-white", "animated", "slideInDown", "text-black");
+  if (type == 'add') {
+    header.classList.remove('absolute', 'text-white', 'nav-black');
+    header.classList.add('fixed', 'bg-white', 'animated', 'slideInDown', 'text-black');
   }
 
-  if (type == "remove") {
-    header.classList.add("absolute", "text-white");
-    header.classList.remove("fixed", "bg-white", "animated", "slideInDown", "text-black");
+  if (type == 'remove') {
+    header.classList.add('absolute', 'text-white', 'nav-black');
+    header.classList.remove('fixed', 'bg-white', 'animated', 'slideInDown', 'text-black');
   }
 }
 
 function toggleHeader(scrollPos) {
   // Use if you only want header fixed on homepage
   // if (page("home") && scrollPos > viewportHeight) {
-  if (page("home")) {
+  if (page('home')) {
     if (scrollPos > viewportHeight) {
-      toggleHeaderClasses("add");
+      toggleHeaderClasses('add');
     } else {
-      toggleHeaderClasses("remove");
+      toggleHeaderClasses('remove');
     }
   } else {
     if (scrollPos > viewportHeight / 3) {
-      toggleHeaderClasses("add");
+      toggleHeaderClasses('add');
     } else {
-      toggleHeaderClasses("remove");
+      toggleHeaderClasses('remove');
     }
   }
 }
 
 toggleHeader();
-window.addEventListener("scroll", function (e) {
+window.addEventListener('scroll', function (e) {
   lastKnownScrollPosition = window.scrollY;
 
   if (!ticking) {
--- resources/js/local/_header.js
+++ resources/js/local/_header.js
@@ -1,63 +1,63 @@
-let lastKnownScrollPosition = 0;
-let ticking = false;
+let lastKnownScrollPosition = 0
+let ticking = false
 var viewportHeight = Math.max(
   document.documentElement.clientHeight,
   window.innerHeight || 0
-);
-let header = document.querySelector(".header");
+)
+let header = document.querySelector('.header')
 
 function toggleHeaderClasses(type) {
-  if (type == "add") {
-    header.classList.remove("absolute", "text-white");
+  if (type == 'add') {
+    header.classList.remove('absolute', 'text-white', 'nav-black')
     header.classList.add(
-      "fixed",
-      "bg-white",
-      "animated",
-      "slideInDown",
-      "text-black"
-    );
+      'fixed',
+      'bg-white',
+      'animated',
+      'slideInDown',
+      'text-black'
+    )
   }
-  if (type == "remove") {
-    header.classList.add("absolute", "text-white");
+  if (type == 'remove') {
+    header.classList.add('absolute', 'text-white', 'nav-black')
     header.classList.remove(
-      "fixed",
-      "bg-white",
-      "animated",
-      "slideInDown",
-      "text-black"
-    );
+      'fixed',
+      'bg-white',
+      'animated',
+      'slideInDown',
+      'text-black'
+    )
   }
 }
 
 function toggleHeader(scrollPos) {
   // Use if you only want header fixed on homepage
   // if (page("home") && scrollPos > viewportHeight) {
-  if (page("home")) {
+  if (page('home')) {
     if (scrollPos > viewportHeight) {
-      toggleHeaderClasses("add");
+      toggleHeaderClasses('add')
     } else {
-      toggleHeaderClasses("remove");
+      toggleHeaderClasses('remove')
     }
   } else {
     if (scrollPos > viewportHeight / 3) {
-      toggleHeaderClasses("add");
+      toggleHeaderClasses('add')
     } else {
-      toggleHeaderClasses("remove");
+      toggleHeaderClasses('remove')
     }
   }
 }
 
-toggleHeader();
+toggleHeader()
 
-window.addEventListener("scroll", function(e) {
-  lastKnownScrollPosition = window.scrollY;
+window.addEventListener('scroll', function(e) {
+  lastKnownScrollPosition = window.scrollY
 
   if (!ticking) {
     window.requestAnimationFrame(function() {
-      toggleHeader(lastKnownScrollPosition);
-      ticking = false;
-    });
+      toggleHeader(lastKnownScrollPosition)
+      ticking = false
+    })
 
-    ticking = true;
+    ticking = true
   }
-});
+})
