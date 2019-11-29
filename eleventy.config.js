09e746bcad42e5f95945dfb284017b9744d44cfa
Nico van Zyl <nicovz92@gmail.com>
2019-11-29 20:45:59 +0200

    Init archive content

--- eleventy.config.js
+++ eleventy.config.js
@@ -31,7 +31,8 @@ module.exports = eleventyConfig => {
 
   // Layout aliases
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk')
+  eleventyConfig.addLayoutAlias('video', 'layouts/video.njk')
+  eleventyConfig.addLayoutAlias('archive', 'layouts/archive.njk')
 
   // Include our static assets
   eleventyConfig.addPassthroughCopy('css')
